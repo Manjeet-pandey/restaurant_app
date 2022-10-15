f96c9f762f401a87aacee8aecf836a36345b15ce
Manjeet Pandey <075bct048.manjeet@pacmpus.edu.np>
2022-10-15 18:18:34 +0545

    Still some changes required

---
+++ customers/views.py
@@ -0,0 +1,182 @@
+from http.client import HTTPResponse
+from random import random
+from statistics import quantiles
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views import View
+from .models import AddOns, Cart, MenuItem, Category , AddOns,Offers, Order,Review
+from .models import Cart as Product
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import OrderForm, UserRegistrationForm,ReviewForm
+from django.http import JsonResponse,HttpResponseRedirect
+from django.core.serializers import serialize
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.db.models import Q
+
+# Create your views here.
+
+class Index(View):
+    def get(self, request, *args , **kwasrgs):
+        menu_items = MenuItem.objects.all()[:6]
+        ingredients = AddOns.objects.all()
+        offers = Offers.objects.all()
+        context = {
+            'menu_items' : menu_items,
+            'ingredients':ingredients,
+            'offers': offers,
+        }
+        return render(request, 'customers/index.html',context)
+
+class About(View):
+    def get(self, request, *args , **kwasrgs):
+        return render(request, 'customers/about.html')
+
+class Offers_view(View):
+    def get(self, request, *args , **kwasrgs):
+        offers= Offers.objects.all()
+        context = {
+            'offers' : offers,
+        }
+        return render(request, 'customers/offers.html',context)
+
+
+class Menu(View):
+    def get(self, request):
+        menu_items = MenuItem.objects.all()
+        
+        
+        context = {
+            'menu_items' : menu_items,
+            
+        }
+        
+        return render(request, 'customers/menu.html', context)
+
+    def post(self, request):
+        user = get_object_or_404(User, request.user)
+
+
+class CartView(View):
+    def get(self, request, *args , **kwasrgs):
+        cart_items = Cart.objects.filter(user=request.user)
+        item_count = cart_items.count()
+        total_price = sum(items.items.price for items in cart_items )
+        context = {
+            'cart_items':cart_items,
+            'item_count': item_count,
+            'total_price': total_price,
+        }
+
+        return render(request, 'customers/add_to_cart.html',context)   
+
+    def post(self, request, *args , **kwargs):
+        cart = Cart.objects.filter(user=request.user)
+        form = OrderForm(request.POST)
+        for items in cart:
+            if form.is_valid():
+                state = request.POST.get('state')
+                city = request.POST.get('city')
+                postal_code = request.POST.get('postal_code')
+                order = Order(cart=items, state=state,  city=city , postal_code=postal_code,is_payed=True)
+                order.save()
+                return render(request,'customers/index.html',{'alert':"Your order will be delivered soon."})
+
+        return render(request, 'customers/add_to_cart.html')
+
+class Product_description(View):
+    def get(self, request,id):
+        product=MenuItem.objects.get(id=id)
+        ingredients = AddOns.objects.all()
+        reviews = Review.objects.filter(menu=product)
+        context = {
+            'product':product,
+            'ingredients' : ingredients,
+            'reviews': reviews
+            
+        }
+        
+        return render(request,'customers/product_description.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('index')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'customers/register.html', context)
+
+@login_required
+def add_to_cart(request, id):
+    item = get_object_or_404(MenuItem,id=id)
+    cart = Cart.objects.create(user=request.user, items=item,price=item.price)
+    cart.save()
+    messages.success(request,'Added to cart')
+    return redirect('cart')
+
+@login_required
+def remove_from_cart(request, id):
+    Cart.objects.filter(id=id).delete()
+    messages.success(request,'REmoved fromo cart')
+    return redirect('cart')
+
+@login_required
+def rate(request, id):
+    post = MenuItem.objects.get(id=id)
+    form = ReviewForm(request.POST or None)
+    if form.is_valid():
+        author = request.user
+        stars = request.POST.get('stars')
+        comment = request.POST.get('comment')
+        review = Review(author=author, stars = stars,  comment=comment , menu=post)
+        review.save()
+        return HttpResponseRedirect(reverse('product', args=(post.id,)))
+
+    form = ReviewForm()
+    context = {
+        "form":form
+
+    }
+
+    return render(request, 'customers/rate.html',context)
+
+
+class Dashboard(View):
+    def get(self, request, *args , **kwargs):
+        orders = Order.objects.all()
+        context = {
+            'orders':orders,
+        }
+        return render(request,'customers/dashboard.html',context)
+    def post(self, request, *args , **kwargs):
+        id= request.POST.get('id')
+        order = Order.objects.get(id=id)
+        delivered = request.POST.get('delivered')
+        if delivered == "on":
+            order.is_delivered =  True
+            order.save()
+        return redirect('dashboard')
+
+def search(request):
+
+    results = []
+
+    if request.method == "GET":
+
+        query = request.GET.get('search')
+
+        if query == '':
+
+            query = 'None'
+
+        results = MenuItem.objects.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(price__icontains=query) )
+
+    return render(request, 'customers/search_results.html', {'query': query, 'results': results})
---
+++ static/js/custom.js
@@ -0,0 +1,109 @@
+
+// isotope js
+$(window).on('load', function () {
+    $('.filters_menu li').click(function () {
+        $('.filters_menu li').removeClass('active');
+        $(this).addClass('active');
+
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: data
+        })
+    });
+
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: false,
+        masonry: {
+            columnWidth: ".all"
+        }
+    })
+   
+    
+});
+
+
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+    $('#rateMe3').mdbRate();
+});
+$('.btnNext').click(function() {
+    $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
+});
+$(function () {
+    // this will get the full URL at the address bar
+    var url = window.location.href;
+    // passes on every "a" tag
+    $(".navbar-nav .nav-link").each(function () {
+      // checks if its the same on the address bar
+      if (url == (this.href)) {
+        $(this).closest("li").addClass("active");
+        //for making parent of submenu active
+        $(this).closest("li").parent().parent().addClass("active");
+      }
+    });
+  });
+// nice select
+$(document).ready(function() {
+    $('select').niceSelect();
+  });
+
+  $(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
+
+function ModelFunction(price){
+    
+    document.getElementById('model').style.display='block';
+    document.getElementById('item_price').innerHTML = price ;
+    document.getElementById('hidden_price').value = price;
+    updatePrice();
+}
+
+
+function updatePrice() {
+    let base_price = parseFloat(document.getElementById("hidden_price").value);
+    const ingredients = new Array()
+    const checkboxes = document.getElementsByClassName("item");
+    for(let i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked){ 
+            base_price += parseFloat(checkboxes[i].value) || 0;
+            ingredients.push(checkboxes[i].id)
+    }
+} 
+    
+    document.getElementById("item_price").innerText = base_price;
+}
+$(".calculate").change(function() {
+    basePrice=parseFloat(document.getElementById("cartHidden").value);
+    newPrice=basePrice;
+    $(".calculate option:selected").each(function() {
+        newPrice += $(this).data('price')
+    });
+    console.log(newPrice)
+    $("#cartTotal").html(newPrice.toFixed(2));
+});
+
